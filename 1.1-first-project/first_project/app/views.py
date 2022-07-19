d75658b90547a5a827da3ccb5122c00d431b04a2
Петр Кравцов <rat.f.br@gmail.com>
2022-07-19 14:53:48 +0300

    Homework 1

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+from datetime import datetime
+from os import listdir
 
 
 def home_view(request):
@@ -8,12 +10,12 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
+    # подробнее о них мы поговорим на следующих лекциях
     context = {
         'pages': pages
     }
@@ -23,7 +25,7 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.now().time()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
@@ -32,4 +34,5 @@ def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    res = str(listdir('./'))
+    return HttpResponse(res)
--- 1.1-first-project/first_project/first_project/settings.py
+++ 1.1-first-project/first_project/first_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # здесь мы подключаем наши приложения
-    'app'
+    'app',
 ]
 
 MIDDLEWARE = [
