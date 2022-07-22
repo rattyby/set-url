b7b3a94c22a68b1aff6aac70852e23f3fe2dae3d
Петр Кравцов <rat.f.br@gmail.com>
2022-07-22 08:42:22 +0300

    Revert "Homework 2"
    This reverts commit 6cfa0eff70aa82c22f233c911ee28705113ce7ce.

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,10 +2,5 @@ from django.db import models
 
 
 class Phone(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50)
-    price = models.PositiveIntegerField
-    image = models.ImageField
-    release_date = models.DateField
-    lte_exists = models.BooleanField
-    slug = models.SlugField
+    # TODO: Добавьте требуемые поля
+    pass
