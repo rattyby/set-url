6cfa0eff70aa82c22f233c911ee28705113ce7ce
Петр Кравцов <rat.f.br@gmail.com>
2022-07-22 08:42:02 +0300

    Homework 2

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.PositiveIntegerField
+    image = models.ImageField
+    release_date = models.DateField
+    lte_exists = models.BooleanField
+    slug = models.SlugField
