0146bf995356bfd6d37bf1109ad3e3136f4750be
Петр Кравцов <rat.f.br@gmail.com>
2022-07-29 09:56:41 +0300

    Homework 4

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,11 +1,14 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
 
-from articles.models import Article
+from articles.models import Article, ArticleTag
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
+    object_list = Article.objects.order_by('-published_at').prefetch_related(
+        Prefetch('scopes', ArticleTag.objects.order_by('-is_main', 'tag__name')))
+    context = {'object_list': object_list}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
